221e5acc0da52eb65356e00a7d78b25980ebd29c
xmasuhai <xmasuhai@163.com>
2021-11-29 13:09:32 +0800

    :sparkles: 添加promise-hell.js示例

--- promise/promise-hell.js
+++ promise/promise-hell.js
@@ -1,14 +1,18 @@
 import thenFs from 'then-fs'
 
+// bad
 thenFs.readFile('./files/path-a.txt', 'utf8')
-  .then(result => {
-    console.log(result) // './files/path-b.txt'
-    thenFs.readFile(result, 'utf8')
-      .then(result => {
-        console.log(result) // './files/path-c.txt'
-        thenFs.readFile(result, 'utf8')
-          .then(result => {
-            console.log(result) // end
+  .then(res => {
+    console.log('bad', res) // './files/path-b.txt'
+    thenFs.readFile(res, 'utf8')
+      .then(res => {
+        console.log('bad', res) // './files/path-c.txt'
+        thenFs.readFile(res, 'utf8')
+          .then(res => {
+            console.log('bad', res) // './files/path-a.txt'
+          })
+          .catch(err => {
+            console.log(err.message)
           })
       })
       .catch(err => {
@@ -18,3 +22,21 @@ thenFs.readFile('./files/path-a.txt', 'utf8')
   .catch(err => {
     console.log(err.message)
   })
+
+// good
+thenFs.readFile('./files/path-a.txt', 'utf8')
+  .then(res => {
+    console.log('good', res)
+    return thenFs.readFile(res, 'utf8')
+  })
+  .then(res => {
+    console.log('good', res)
+    return thenFs.readFile(res, 'utf8')
+  })
+  .catch (err => {console.log(err)})
+
+// abbr
+thenFs.readFile('./files/path-a.txt', 'utf8')
+  .then(res => thenFs.readFile(res, 'utf8'))
+  .then(res => thenFs.readFile(res, 'utf8'))
+  .catch (err => {console.log(err)})

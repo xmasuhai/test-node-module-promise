4b510f96fd4677a4235d279bf849988197540a79
xmasuhai <xmasuhai@163.com>
2021-12-02 16:30:29 +0800

    :sparkles: 添加 coinflip-then.js 示例

--- promise/coinflip-then.js
+++ promise/coinflip-then.js
@@ -1,29 +1,38 @@
 export const coinFlip = (betNum) => {
   return new Promise((resolve, reject) => {
-    const hasWon = Math.random() > 0.5;
+    const hasWon = Math.random() > 0.5
     hasWon
       ? setTimeout(() => {
-        resolve(betNum * 2);
+        resolve(betNum * 2)
       }, 2000)
-      : reject(new Error("Sorry, You lost...")); // same as -> throw new Error ("You lost ...");
-  });
-};
+      : reject(new Error('Sorry, You lost...')) // same as -> throw new Error ("You lost ...");
+  })
+}
 
 export const betAgain = (result) => {
-  console.log(`CONGRATULATIONS! YOU'VE WON ${result}!`);
-  console.log(`LET'S BET AGAIN!`);
-  return coinFlip(result);
-};
+  console.log(`CONGRATULATIONS! YOU'VE WON ${result}!`)
+  console.log(`LET'S BET AGAIN!`)
+  return coinFlip(result)
+}
 
 export const handleRejection = (e) => {
-  console.log(e.message);
-};
+  console.log(e.message)
+}
 
 coinFlip(10)
   .then(betAgain)
   .then(betAgain)
   .then(betAgain)
   .then(result => {
-    console.log(`OMG, WE DID THIS! TIME TO TAKE ${result} HOME!`);
+    console.log(`OMG, WE DID THIS! TIME TO TAKE ${result} HOME!`)
   })
-  .catch(handleRejection);
+  .catch(handleRejection)
+
+coinFlip(10)
+  .then(coinFlip)
+  .then(coinFlip)
+  .then(coinFlip)
+  .then(result => {
+    console.log(`OMG, WE DID THIS! TIME TO TAKE ${result} HOME!`)
+  })
+  .catch(handleRejection)

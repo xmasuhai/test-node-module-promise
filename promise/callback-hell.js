e7f114217500c866b30a8ab976bc6ac29510ec27
xmasuhai <xmasuhai@163.com>
2021-12-04 12:24:40 +0800

    :sparkles: 添加 callback hell 的示例

---
+++ promise/callback-hell.js
@@ -0,0 +1,19 @@
+// 延时1秒后执行函数
+const delay = (time/* : number */, callback/* : Function */) => {
+  setTimeout(() => {
+    callback()
+  }, time)
+}
+
+delay(1000, () => {
+  console.log('第一次执行')
+
+  delay(2000, () => {
+    console.log('第二次执行')
+
+    delay(3000, () => {
+      console.log('第三次执行')
+
+    })
+  })
+})

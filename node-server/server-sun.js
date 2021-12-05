8ab874bad0f866f77a45bd0e10cc48149ac11e5c
xmasuhai <xmasuhai@163.com>
2021-12-05 23:31:27 +0800

    :sparkles: 添加 node-server 关于数据Mock、前后端联调 的示例

---
+++ node-server/server-sun.js
@@ -0,0 +1,19 @@
+// 加载 node 模块
+import http from 'http' // 创建服务器
+import fs from 'fs' // 文件读取模块
+import * as path from 'path'
+
+const __dirname = path.resolve(path.dirname(''))
+
+const server = http.createServer((req /*请求对象*/, res /*响应对象*/) => {
+  let urlObj = new URL(`${req.url}`, 'http://localhost:8080/')
+
+  if (urlObj.pathname === '/getWeather') {
+    res.end(JSON.stringify({data: '晴天'}))
+  } else {
+    res.end(fs.readFileSync(__dirname + '/index.html'))
+  }
+
+}).listen(8080, () => {
+  console.log('点击打开： http://localhost:8080/index.html')
+}) // 监听端口
--- node-server/server.js
+++ node-server/server.js
@@ -1,12 +1,37 @@
 // 加载 node 模块
+/*
 const http = require('http') // 创建服务器
 const fs = require('fs') // 文件读取模块
 const url = require('url') // url解析模块 将url字符串解析成一个对象
+ */
+// 转换为ES6模块
+import http from 'http' // 创建服务器
+import fs from 'fs' // 文件读取模块
+import * as path from 'path'
+
+const __dirname = path.resolve(path.dirname(''))
 
 const server = http.createServer((req /*请求对象*/, res /*响应对象*/) => {
-  let urlObj = url.parse(req.url)
+  let pathObj = new URL(`${req.url}`, 'http://localhost:8080/')
+  // console.log(pathObj)
+  // console.log(pathObj.searchParams.get('city'))
+  if (pathObj.pathname === '/getWeather') {
+    pathObj.searchParams.get('city') === 'beijing'
+      ? res.end(JSON.stringify({city: '北京', weather: '晴天'}))
+      : res.end(JSON.stringify({city: pathObj.searchParams.get('city'), weather: '未知'}))
+  } else {
+    try {
+      // 默认为index.html
+      let pathname = pathObj.pathname === '/' ? '/index.html' : pathObj.pathname
+      // 读取非index.html文件的内容
+      res.end(fs.readFileSync(__dirname + pathname))
+    } catch (e) {
+      res.writeHead(404, 'Note Found')
+      res.end('<h1>404 Page Not Found</h1>')
+    }
 
-  // 读取文件
-  res.end(fs.readFileSync(__dirname + '/index.html'))
+  }
 
-}).listen(8080) // 监听端口
+}).listen(8080, () => {
+  console.log('点击打开： http://localhost:8080/index.html')
+}) // 监听端口

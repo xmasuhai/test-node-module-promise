522f2cbd4958eb71ae092103f79eef6a5fdc2c43
xmasuhai <xmasuhai@163.com>
2021-12-07 16:48:09 +0800

    :sparkles: 添加 promise-ten.js  asyncAwait.js 示例

---
+++ async_await/asyncAwait.js
@@ -0,0 +1,42 @@
+const sleep = () => {
+  return new Promise(resolve => {
+    setTimeout(() => {
+        return resolve(300)
+      },
+      1000)
+  })
+};
+
+;(async () => {
+  // await 后面放置的不是一个 Promise 实例
+  // 则浏览器默认会把其转换为一个“状态为成功
+  // 值就是 await 后的值”的 promise 实例
+  let result = await 1 // await Promise.resolve(1)
+  console.log('result 1st', result)
+
+  result = await Promise.resolve(2)
+  console.log('result 2nd', result)
+
+  result = await sleep()
+  console.log('result 3rd', result)
+  // 先 sleep 执行
+  // 把返回的 promise 实例放在 await 后面等着
+  // 当前案例只有 1000ms 后，才能知道实例状态
+})()
+
+/*
+result 1st 1
+result 2nd 2
+result 3rd 300
+* */
+
+/*
+- await 需要等待**后面的 promise 实例是状态为成功**时，才会执行之后的代码
+- 首先 当前上下文中，await 之后的代码都是**异步微任务** @aw
+- 如果已经知道 await 后面的实例状态是成功的话
+  - 则 @aw 直接放在 Event Queue 中，等待执行即可
+- 如果 await 后面实例状态是失败的话
+  - 则 @aw 在 Web API 中永远不会进入到 Event Queue 中，因为永远不会执行
+- 如果暂时还不知道是成功还是失败，则 @aw 先放置在 Web API 中
+  - 等到知道实例状态是成功后，再挪至到 Event Queue 中等待执行
+* */
---
+++ promise/promise-then.js
@@ -0,0 +1,50 @@
+// Promise.resolve 返回一个带着给定值 解析过的 Promise 对象
+// 如果参数本身就是一个 Promise 对象，则直接返回这个 Promise 对象
+let p1 = Promise.resolve(100)
+
+console.log('p1', p1) // p1 Promise {<fulfilled>: 100}
+
+let p2 = p1
+  // p1.then 时，此处的 onfulfilled 方法放在 Event Queue 中的微任务队列等待执行（@A）
+  .then(result => {
+    console.log(`成功: ${result}`)
+    console.log('p2 1st', p2)
+    return result * 10
+  })
+
+console.log('p2 2nd', p2) // p2 2nd Promise {<pending>}
+
+p2
+  // 此时还不知道 p2 的状态，把 onfulfilled 放在 Event Queue 中的微任务队列等待执行（@B）
+  .then(result => {
+    console.log(`成功: ${result}`)
+    console.log('p2 3rd', p2)
+  })
+
+console.log('SYNC END')
+
+// 同步代码结束后，开始执行@A -> 成功：100 ->
+// 并且修改 p2 的状态为 成功，值是1000； ->
+// 此时 @B 可以执行了 ->
+// 把 @B 也放在等待的异步微任务队列中 ->
+// 如果没有其他的异步任务执行，这把@B也拿出来执行 ->
+// 成功：1000
+
+/*
+p1 Promise { 100 }
+p2 2nd Promise { <pending> }
+SYNC END
+成功: 100
+p2 1st Promise { <pending> }
+成功: 1000
+p2 3rd Promise { 1000 }
+* */
+
+/*
+基于then返回的 promise 实例的状态和值，主要看 onfulfilled/onrejected 是否执行
+- 函数返回的不是 promise 实例：
+  - 方法执行不报错，p2 状态是 成功，值即返回值
+  - 方法执行报错，则 p2 是失败的，值是报错原因
+- 函数返回的是个 promise 实例：则这个实例的状态和值决定了 p2 的状态和值
+*/
+
